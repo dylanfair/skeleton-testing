72b26a376642f0e2009c3b0b9b29ab9a908d5ec6
Dylan <carringtonfair.dylan@gmail.com>
2023-05-08 00:35:21 -0400

    Minor changes

--- my-skeleton-app/src/routes/+page.svelte
+++ my-skeleton-app/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="container mx-auto p-8 space-y-8">
 	<h1>My First Skeleton App</h1>
-	<p>This static site will generally hold whatever I am currently experimenting on within the skeleton framework on <a href="https://pages.github.com/" target="_blank">GitHub Pages</a>.</p>
+	<p>This static site hosted on <a href="https://pages.github.com/" target="_blank">GitHub Pages</a> will generally hold whatever I am currently experimenting with in the skeleton framework.</p>
 	<p>
-		The goal is that through working on this site I will learn more about using <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a> with <a href="https://www.skeleton.dev/" target="_blank">SkeletonUI</a>.
+		The goal is that through working on this site I will learn more about using both <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a> with <a href="https://www.skeleton.dev/" target="_blank">SkeletonUI</a>.
 	</p>
 </div>
