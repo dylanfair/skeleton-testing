8686c4fe0ca8a29c293a358c475d2c7be2c4e802
Dylan <carringtonfair.dylan@gmail.com>
2023-05-09 21:20:08 -0400

    gonna test at site

--- my-skeleton-app/src/routes/+layout.svelte
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -8,9 +8,13 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile } from '@skeletonlabs/skeleton';
 	import { Modal, Toast } from '@skeletonlabs/skeleton'
 	import { Avatar } from '@skeletonlabs/skeleton';
 
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
+	const selected = derived(page, $page => $page.url.pathname);
 
 	function avatarClick() {
 		window.open(
@@ -24,27 +28,33 @@
 <Modal />
 
 <!-- App Shell -->
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
-
-	<svelte:fragment slot="sidebarLeft">
-		<!-- Insert the list: -->
-		<nav class="list-nav">
-			<ul>
-				<li><a href="{base}/">Home</a></li>
-				<li><a href="{base}/notes">Notes</a></li>
-				<li><a href="{base}/data" data-sveltekit-preload-data="hover">Data</a></li>
-			</ul>
-		</nav>
-		<!-- --- -->
-	</svelte:fragment>
+<AppShell>
 
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar gridColumns="grid-cols-3"  slotDefault=place-self-center slotTrail="place-content-end">
 			<svelte:fragment slot="lead">
 				<strong class="text-xl uppercase hover:text-primary-500"><a href="https://www.skeleton.dev/" target="_blank">Skeleton</a></strong>
 			</svelte:fragment>
-			<svelte:fragment slot="trail">
+
+			<svelte:fragment>
+				<AppRail {selected}>
+					<AppRailTile label="Home" href="{base}/"><i class="fa-solid fa-image text-2xl" /></AppRailTile>
+					<AppRailTile label="Notes" href="{base}/notes"><i class="fa-solid fa-image text-2xl" /></AppRailTile>
+					<AppRailTile label="Data" href="{base}/data" data-sveltekit-preload-data="hover"><i class="fa-solid fa-image text-2xl" /></AppRailTile>
+				</AppRail>
+					
+				
+				
+			
+				<!-- <div class="flex flex-none items-center space-x-20">
+					<a class="space-x-5 btn hover:variant-soft-primary" href="{base}/">Home</a>
+					<a class="space-x-5 btn hover:variant-soft-primary" href="{base}/notes">Notes</a>
+					<a class="space-x-5 btn hover:variant-soft-primary" href="{base}/data" data-sveltekit-preload-data="hover">Data</a>
+				</div>			 -->
+			</svelte:fragment>
+
+			<svelte:fragment slot="trail">	
 				<a
 					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/dylanfair/skeleton-testing"
---
+++ my-skeleton-app/src/routes/data/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+    <svelte:fragment slot="sidebarLeft">
+        <!-- Insert the list: -->
+        <nav class="list-nav">
+            <ul>
+                <li><a href="#products">Top Products</a></li>
+                <li><a href="#users">Top Users</a></li>
+            </ul>
+        </nav>
+        <!-- --- -->
+    </svelte:fragment>    
+    <slot />
+</AppShell>
